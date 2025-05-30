256025a8b5b438f28cc7801c343368e3321e04ce
Usuario <Usuario@equipowindows>
2025-05-30 09:42:19 +0200

    Segundo change

--- Proyecto1/src/paquete1/main.java
+++ Proyecto1/src/paquete1/main.java
@@ -5,7 +5,13 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Control de versiones de git");
+		try {
+			System.out.println("Control de versiones de git");
+			System.out.println("Segundo cambio");
+			
+		}catch(Exception e) {
+			
+		}
 		
 	}
 
